43a755b27bc7b068846926a4682afbc7a4af357b
ozaamm <villeoskari15@gmail.com>
2025-04-02 23:55:40 +0300

    Cleaner UI and removed unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styles from './home.module.css';
 import filterStyles from '../components/FiltersContainer.module.css';
 import CharacterCard from '../components/CharacterCard';
